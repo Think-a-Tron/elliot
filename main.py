371a6605af8b7842fb7d9c700e09012c2b798b9d
ExpressGradient <johndoeknown@protonmail.com>
2025-08-24 13:30:59 +0530

    adds context coverage node

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import argparse
 from os import environ
 from pathlib import Path
-from typing import TypedDict
+from typing import Literal, TypedDict, List
 
 from langgraph.graph import END, START, StateGraph
 from openai import OpenAI
+from pydantic import BaseModel, Field
 from rich import print
 from rich.markdown import Markdown
 from rich.panel import Panel
 
-from tools import ripgrep_spec
-
-MODEL_NAME = environ.get("MODEL_NAME", "openai/gpt-oss-120b")
+MODEL_NAME = environ.get("MODEL_NAME", "mistralai/mistral-medium-3.1")
 BASE_URL = environ.get("BASE_URL", "https://openrouter.ai/api/v1")
 
 client = OpenAI(base_url=BASE_URL)
@@ -19,11 +18,16 @@ client = OpenAI(base_url=BASE_URL)
 
 class InputState(TypedDict):
     issue: str
-    repo_path: str
+
+
+class ContextSchema(TypedDict):
+    repo_root: str
 
 
 class State(TypedDict):
     understanding: str
+    context: str
+    feedback_to_scout: str
 
 
 def create_understanding(state: InputState):
@@ -40,6 +44,7 @@ Issue:
 
 Task:
 Create a clear, minimal but complete understanding of the issue.
+
 Include:
 - Restate the issue in plain terms (what it is really about).
 - Clarify its type (bug, feature request, question, refactor, docs, etc.).
@@ -60,7 +65,7 @@ Keep it concise and technical, but make sure no important detail is lost.""",
     print(
         Panel(
             Markdown(understanding),
-            title="Understanding",
+            title="Issue Understanding",
             subtitle=f"Tokens {total_tokens}",
             border_style="blue",
         )
@@ -69,19 +74,59 @@ Keep it concise and technical, but make sure no important detail is lost.""",
     return {"understanding": completion.choices[0].message.content}
 
 
-def create_code_scan_actions(state: State):
-    client.chat.completions.create(
+class CoverageResponse(BaseModel):
+    decision: Literal["more_context_needed", "enough_context"] = Field(
+        default="more_context_needed"
+    )
+    feedback: List[str] = Field(description="List of search items to scout for")
+
+
+def context_coverage(state: State):
+    understanding = state.get("understanding")
+    context = state.get("context", "none")
+
+    completion = client.chat.completions.parse(
         model=MODEL_NAME,
-        messages=[{"role": "user", "content": ""}],
-        tools=[ripgrep_spec],
+        messages=[
+            {
+                "role": "user",
+                "content": "Given an understanding of a GitHub issue,"
+                "check if we have enough information/context about the codebase to plan for this issue. "
+                "Give feedback of what else we need from the repo, if current context isn't enough. "
+                "Feedback should be a list of targeted search items and their justifications."
+                f"Issue Understanding: {understanding}\n\n"
+                f"Current Context about the Code: {context}",
+            }
+        ],
+        response_format=CoverageResponse,
     )
 
+    coverage = completion.choices[0].message.parsed
+    total_tokens = completion.usage.total_tokens if completion.usage else 0
 
-builder = StateGraph(State, input_schema=InputState)
+    if isinstance(coverage, CoverageResponse):
+        if coverage.decision == "more_context_needed":
+            print(
+                Panel(
+                    Markdown("\n".join(f"- {item}" for item in coverage.feedback)),
+                    title="Context Coverage",
+                    subtitle=f"Tokens {total_tokens}",
+                    border_style="yellow",
+                )
+            )
+
+            return {"feedback_to_scout": coverage.feedback}
+
+    return {"feedback_to_scout": []}
+
+
+builder = StateGraph(State, input_schema=InputState, context_schema=ContextSchema)
 builder.add_node(create_understanding)
+builder.add_node(context_coverage)
 
 builder.add_edge(START, "create_understanding")
-builder.add_edge("create_understanding", END)
+builder.add_edge("create_understanding", "context_coverage")
+builder.add_edge("context_coverage", END)
 
 graph = builder.compile()
 
@@ -95,7 +140,7 @@ def main():
     with open(args.issue_file) as file:
         issue = file.read()
 
-    graph.invoke({"issue": issue, "repo_path": str(args.repo.resolve())})
+    graph.invoke({"issue": issue}, context={"repo_root": args.repo.resolve()})
 
 
 if __name__ == "__main__":
